8669f5533df7b76468dfec4657d1b27440771db2
Martijn van Gogh <martijn@traveladventuregames.nl>
2016-09-07 13:28:15 +0200

    Eerste commit

--- NewProjectForGitHub/ViewController.swift
+++ NewProjectForGitHub/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +22,10 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func Commit(sender: AnyObject) {
+        
+        myLabel.text = "Pressed!"
+        
+    }
 }
 
